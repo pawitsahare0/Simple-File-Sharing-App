7d30deec01708bfa32ce82aceb9bd65f1c0acae2
rdpofgit <121335791+rdpofgit@users.noreply.github.com>
2023-01-16 14:26:44 +0530

    Update code.js

--- public/code.js
+++ public/code.js
@@ -43,7 +43,7 @@
 			shareFile({
 				filename: file.name,
 				total_buffer_size:buffer.length,
-				buffer_size:1098,
+				buffer_size:6098,
 			}, buffer, el.querySelector(".progress"));
 		}
 		reader.readAsArrayBuffer(file);
